ccd034aac045a4e327c69388040b8a280a8744fb
Aashishgiri27 <152009034+Aashishgiri27@users.noreply.github.com>
2025-06-07 16:25:04 +0530

    Update Adminproduct.jsx

--- reactapp/src/componemt/Adminpage/Adminproduct.jsx
+++ reactapp/src/componemt/Adminpage/Adminproduct.jsx
@@ -11,7 +11,7 @@ function Adminproduct() {
 
   const getData = async () => {
     try {
-      const response = await axios.get("/product");
+      const response = await axios.get(baseUrl + "/product");
       setData(response.data);
     } catch (error) {
       console.error("Error fetching data", error);
